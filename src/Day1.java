4f69d249c70ed994a35ce477100ed3bebbab98f8
Ashish-Rajbhandari <ashishrajbhandari64@gmail.com>
2021-02-09 22:48:14 -0800

    initial commit

---
+++ src/Day1.java
@@ -0,0 +1,15 @@
+public class Day1 {
+    public static void main(String[] args) {
+        int i = 2;
+        int j = 12;
+
+        i = i +j;
+        j = i - j;
+        i = i -j;
+        System.out.println(i);
+        System.out.println(j);
+
+
+    }
+
+}
