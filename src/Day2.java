f6d9380794fc2a187d677f421b33cb1cb0159d3b
rajeshbohara <rbohara1@mail.ccsf.edu>
2021-02-11 02:22:10 -0800

    My second work  commit

---
+++ src/Day2.java
@@ -0,0 +1,11 @@
+import java.util.Locale;
+
+public class Day2 {
+    public static void main(String[] args) {
+        String name = "rajesh";
+        String lastname = "bohara";
+        System.out.println(name.toUpperCase());
+        System.out.println(lastname.toLowerCase());
+
+    }
+}
---
+++ src/Day2hw.java
@@ -0,0 +1,12 @@
+
+
+public class Day2hw {
+    public static void main(String[] args) {
+        String word = " this is my name ";
+        System.out.println(" The length of word with space "+ word.length());
+        String newWord = word.replace(" ","");
+        System.out.println(" The length of the word without space " +newWord.length());
+    }
+}
+
+
